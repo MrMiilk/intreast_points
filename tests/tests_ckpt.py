a03bac7858d20e57c469efc0ac24c904ff1baa3f
MrMiilk <1024109095@qq.com>
2018-09-10 21:33:42 +0800

    test part

--- tests/tests_ckpt.py
+++ tests/tests_ckpt.py
@@ -1,17 +1,25 @@
+import sys
+import os
+
+path = "/home/int_point/"
+sys.path.append(path)
 from magic_point import *
 
 
 if __name__ == '__main__':
+    tf.reset_default_graph()
     input_shape = [None, 240, 320, 3]
     label_shape = [None, 240, 320, 1]
-    batch_size = 4
-    ckpt_name = ''
+    batch_size = 1
+    ckpt_name = 'checkpoints/-15000'
     Model = Magic_point()
     saver = tf.train.Saver()
     Model.model(input_shape, label_shape, opt='sgd', lr=0.001, training=False)
     with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
         saver.restore(sess, ckpt_name)
         loss = 0.
+        i = 0
         for X, label in get_batch(batch_size, iter=1, img_p=TEST_IMAGE_PATH, pot_p=POINT_PATH):
             feed_dict = {
                 Model.inputs: X,
@@ -20,5 +28,6 @@ if __name__ == '__main__':
             }
             point_position, loss_now = sess.run((Model.point_position, Model.loss), feed_dict=feed_dict)
             ##save point position##
-            np.save('', point_position)
+            np.save('./test_outputs/' + str(i) + '.npy', point_position)
+            i += 1
             loss += loss_now # 好像没什么意义
